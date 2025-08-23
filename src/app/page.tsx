1f100b77898aafd7c35001ae6d70c0c797bd4e7f
Canyen Palmer <Canyen2019@gmail.com>
2025-08-22 21:01:38 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,5 @@
-import Hero from "@/components/Hero";
-export default function Page() { return (<><Hero /></>); }
+import Hero from "../components/Hero";
+
+export default function Page() {
+  return <Hero />;
+}
