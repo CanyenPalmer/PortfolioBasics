64d03dbcdff79dfb86491423363c1fa0ef361d58
Canyen Palmer <Canyen2019@gmail.com>
2025-09-12 12:46:00 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  // TEMP: let the build succeed even if TS/ESLint complains.
-  // Remove these once you’re ready to enforce checks.
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+  reactStrictMode: true
 };
 
 export default nextConfig;
