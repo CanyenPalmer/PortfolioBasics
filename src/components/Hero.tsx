3e0c250974e2bd1218bba57166da8aa3adf09cd7
Canyen Palmer <Canyen2019@gmail.com>
2025-09-17 17:52:47 -0400

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -15,18 +15,7 @@ export default function Hero({ headline, subheadline, typer }: Props) {
       aria-label="Hero"
       className="relative bg-[#0b1016] overflow-hidden"
     >
-      {/* Background typography underlay */}
-      <div
-        aria-hidden="true"
-        className="pointer-events-none absolute inset-0 overflow-hidden"
-      >
-        <div className="absolute -top-10 left-6 text-[10rem] font-black tracking-tight text-white/5 select-none">
-          DATA
-        </div>
-        <div className="absolute bottom-0 right-6 text-[9rem] font-black tracking-tight text-white/5 select-none rotate-6">
-          SCIENCE
-        </div>
-      </div>
+      {/* Removed background underlay */}
 
       <div className="container mx-auto px-6 py-24 max-w-7xl relative z-10">
         <motion.h1
